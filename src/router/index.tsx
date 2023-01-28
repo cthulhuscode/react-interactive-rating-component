06d2df29abb1cb5378adc0e7b692435ee1c38555
Enrique Ruvalcaba <jordison352@gmail.com>
2023-01-28 12:20:11 -0600

    Changed createBrowserRouter for createHashRouter to make the app work on gh pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,7 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { Rate, Thank, Error } from "../pages";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <Rate />,
